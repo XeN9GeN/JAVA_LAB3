c88f58767ac4ce0e3f34302a6070a3b07cd405c2
Xeno <k.vanin@g.nsu.ru>
2026-03-11 23:48:30 +0700

    added a func that write your high score in file and load it after new game

--- src/main/java/Extra/HighScore.java
+++ src/main/java/Extra/HighScore.java
@@ -1,13 +1,46 @@
 package Extra;
 
+import java.io.*;
+import java.nio.charset.Charset;
+
 public class HighScore {
     private int high_score;
+    private static final String FILENAME = "high_score.txt";
 
+    public HighScore(){
+        loadHighScore();
+    }
     public int getHigh_score() {
         return high_score;
     }
-
     public void setHigh_score(int high_score) {
         this.high_score = high_score;
+        saveHighScore();
     }
+
+    public void loadHighScore(){
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader
+                (new FileInputStream(FILENAME),Charset.forName("Windows-1251")))){
+
+            String line = reader.readLine();
+            if(line!=null){
+                this.high_score=Integer.parseInt(line.trim());
+            }
+
+        } catch (IOException | NumberFormatException e) {
+            System.err.println("Ошибка при загрузке рекорда: " + e.getMessage());
+            this.high_score = 0;
+        }
+    }
+    public void saveHighScore(){
+        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter
+                (new FileOutputStream(FILENAME),Charset.forName("Windows-1251")))){
+
+            writer.write(String.valueOf(high_score));
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,18 @@ import Model.*;
 import View.InfoPanel;
 
 import javax.swing.*;
+import java.io.*;
+import java.nio.charset.Charset;
 
 public class Main {
     public static void main(String[] args) {
+        String h_score_file = args[0];
+        try(Reader reader = new InputStreamReader(new FileInputStream(h_score_file), Charset.forName("Windows-1251"))) {
+            int h_score=reader.read();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         SwingUtilities.invokeLater(()->{
             GameModel gm = new GameModel();
             GamePanel gp= new GamePanel(gm);
