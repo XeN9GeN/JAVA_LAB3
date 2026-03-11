ed18effa0283d029fc211a807c7975f506fd4617
Xeno <k.vanin@g.nsu.ru>
2026-03-11 12:09:15 +0700

    Random commit in Main to check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        int a =5;
         SwingUtilities.invokeLater(()->{
             GameModel gm = new GameModel();
             GamePanel gp= new GamePanel(gm);
