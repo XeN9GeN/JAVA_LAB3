5e46362207855ba4448c159a2d1aede8f1aadd3e
Xeno <k.vanin@g.nsu.ru>
2026-03-11 12:13:25 +0700

    Add Loger for info

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        int a =5;
         SwingUtilities.invokeLater(()->{
             GameModel gm = new GameModel();
             GamePanel gp= new GamePanel(gm);
