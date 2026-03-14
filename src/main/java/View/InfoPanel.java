85420497200228089e3c35297a00907372492085
Xeno <k.vanin@g.nsu.ru>
2026-03-14 20:46:29 +0700

    /

--- src/main/java/View/InfoPanel.java
+++ src/main/java/View/InfoPanel.java
@@ -32,17 +32,11 @@ public class InfoPanel extends JPanel{
         gr2.drawString("HIGH SCORE", 110, 50);
         gr2.drawString(String.valueOf(mod.getHighScore()),110,70);
 
-        int x = 20;      // Отступ слева
-        int yStart = 350; // Начальная высота для списка управления
-        int step = 25;   // Расстояние между строками
-
         gr2.drawString("CONTROLS:", 20, 350);
         gr2.drawString("Left:  <-", 20, 375);
         gr2.drawString("Right: ->", 20, 400);
         gr2.drawString("Down:   ↓", 20, 425);
         gr2.drawString("Rotate: ↑", 20, 450);
-
-        // Добавляем ваши клавиши
         gr2.drawString("Pause: ESC", 20, 475);
         gr2.drawString("New Game: F1", 20, 500);
 
